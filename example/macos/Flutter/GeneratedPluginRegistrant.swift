e5857f39b101babb8fb2f133978185de0340c4d0
ecorradini <enrico.corradini94@gmail.com>
2021-12-24 15:25:11 +0100

    android

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import http_session
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  HttpSessionPlugin.register(with: registry.registrar(forPlugin: "HttpSessionPlugin"))
+}
